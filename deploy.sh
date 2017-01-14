1b69dc69d8a15395dcaec82ae930810cd34629b0
Domenic Denicola <d@domenic.me>
2017-01-13 19:52:06 -0500

    Use consistent filenames for deploy key

--- deploy.sh
+++ deploy.sh
@@ -89,7 +89,7 @@ if [ "$1" != "--local" ]; then
     ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
     ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
     ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-    openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in console_spec_id_rsa.enc -out console_spec_id_rsa -d
+    openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
     chmod 600 deploy_key
     eval `ssh-agent -s`
     ssh-add deploy_key
